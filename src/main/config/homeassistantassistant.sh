e93c71441f6493c5cef243ae057b55baa88cda51
Bengt Martensson <barf@bengt-martensson.de>
2022-12-25 11:13:31 +0100

    Improved wrapper

--- src/main/config/homeassistantassistant.sh
+++ src/main/config/homeassistantassistant.sh
@@ -3,5 +3,7 @@
 JAVA=java
 HOME="$(dirname -- "$(readlink -f -- "${0}")" )"
 JAR=${HOME}/HomeAssistantAssistant-*-jar-with-dependencies.jar
+HA_HOST=homeassistant
+TOKEN=@${HOME}/token.txt
 
-exec ${JAVA} -jar ${JAR}  "$@"
+exec ${JAVA} -jar ${JAR} --host ${HA_HOST} --token ${TOKEN}  "$@"
